4a4dc8644a5e1834257335942bc4f4a07db9f2b4
alie <anthonylie94@gmail.com>
2023-01-27 01:47:15 +0800

    rendered data into HTML

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 
 
 function getApiData() {
+    const id = document.querySelector("#ID").value;
 
+    fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(response => response.json())
+      .then((data) => {
+        
+        document.querySelector(".card-body").innerHTML = `
+        
+        <h5 class="card-title">${data.userId}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">${data.id}</h6>
+              <p class="card-text">${data.title}</p>
+              <a href="#" class="card-link">${data.completed}</a>  
+        `;
+      })
+      .catch((err)=> {
+        console.log("ID  not found", err);
+      })
+};
+
+function API() {
     fetch('https://jsonplaceholder.typicode.com/todos')
       .then(response => response.json())
       .then(json => console.log(json))
 };
 
-getApiData();
+API();
+
+
